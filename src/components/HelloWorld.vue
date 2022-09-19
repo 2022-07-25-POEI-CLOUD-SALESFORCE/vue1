959d58bcab0720554194a601fa1d282260997da9
Bruno-TAVERNIER <bruno@tavernier.eu>
2022-09-19 11:48:39 +0200

    exercice 4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,10 +5,21 @@
     <h2 v-show="afficheVille">{{ ville }}</h2>
     <p v-if="afficheDate">{{ dateJour }}</p>
     <p v-show="afficheDate">{{ dateJour }}</p>
+    <!-- boucle d'affichage -->
+    <span v-for="i in 10" :key="i">Nombre {{ i }}<br /></span>
+    <!-- avec un tableau i pour l'élément du tableau, j pour l'indice -->
+    <span v-for="(i, j) in [3, 1, 7, 10, 12]" :key="j">
+      Valeur de i: {{ i }} ayant l'index {{ j }}<br />
+    </span>
+    <!-- une boucle ul/li avec le tableau dans "data"-->
+    <ul>
+      <li v-for="(i, j) in tableau" :key="j">{{ i }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+// https://github.com/2022-07-25-POEI-CLOUD-SALESFORCE/vue1.git
 export default {
   name: 'HelloWorld',
   data() {
@@ -18,7 +29,8 @@ export default {
       ville: 'Montpellier',
       dateJour: new Date().toLocaleDateString(),
       afficheDate: true,
-      afficheVille: true
+      afficheVille: true,
+      tableau: ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
     }
   }
 }
@@ -38,5 +50,8 @@ p {
   padding: 10px;
   border: 1px solid silver;
   box-shadow: 2px 2px 2px rgba(0,0,0,.4);
+}
+ul {
+  list-style-type: none;
 }
 </style>
