5466fd30c5b5db6f30e8956344d43f5e9b863524
Bruno-TAVERNIER <bruno@tavernier.eu>
2022-09-19 11:27:34 +0200

    exercice 3

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- pour lier un attribut HTML avec une variable JS on utilise 'v-bind:' ou la version courte ':' -->
+  <img v-bind:alt="msg" src="./assets/logo.png">
+  <p :style="test">{{ msg }}</p>
+  <HelloWorld />
+</template>
+
+<script>
+  //pour utiliser un component il faut l'importer
+import HelloWorld from './components/HelloWorld.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    // pour utiliser un component il faut le déclarer
+    HelloWorld
+},
+  data() {
+    return {
+      msg: 'Hello M2I',
+      test: {color: 'red'}
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ nom}} {{ prenom }}</h1>
+    <h2 v-if="afficheVille">{{ ville }}</h2>
+    <h2 v-show="afficheVille">{{ ville }}</h2>
+    <p v-if="afficheDate">{{ dateJour }}</p>
+    <p v-show="afficheDate">{{ dateJour }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      nom: 'TAVERNIER',
+      prenom: 'Bruno',
+      ville: 'Montpellier',
+      dateJour: new Date().toLocaleDateString(),
+      afficheDate: true,
+      afficheVille: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 3em; 
+  text-shadow: 3px 3px 3px lime;
+}
+h2 {
+  font-size: 2.5em;
+  color: blue;
+}
+p {
+  padding: 10px;
+  border: 1px solid silver;
+  box-shadow: 2px 2px 2px rgba(0,0,0,.4);
+}
+</style>
