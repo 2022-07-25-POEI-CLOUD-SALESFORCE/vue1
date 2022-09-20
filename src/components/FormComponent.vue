eb8df5afd8244fd199f5c39948306e0bf87efd92
Bruno-TAVERNIER <bruno@tavernier.eu>
2022-09-20 14:34:32 +0200

    fin formulaire

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -8,8 +8,13 @@
              v-model.trim : suppression des espaces avant et après le texte
              v-model.number : nombre uniquement
         -->
-        <input v-model.lazy="nom" placeholder="votre nom ici" />
-        <span> {{ nom }} </span>
+        <input v-model.lazy="nom" placeholder="votre nom ici" :class='{ko: nomKo}' />
+        <span v-show="nomKo" class="ko">Votre nom doit comporter au moins 2 caractères...</span>
+      </p>
+      <p>
+        <label>Prénom</label>
+        <input v-model="prenom" placeholder="votre prénom ici" :class='{ko: prenomKo}' />
+        <span v-show="prenomKo" class="ko">Votre prénom doit comporter au moins 2 caractères...</span>
       </p>
       <p>
         <label>Civilité</label>
@@ -23,6 +28,7 @@
       <p>
         <button type="submit">Go !</button>
       </p>
+      <p>{{ message }}</p>
     </form>
   </div>
 </template>
@@ -33,17 +39,34 @@
     data() {
       return {
         nom: 'Toto',
-        civilite: ''
+        prenom: '',
+        civilite: '',
+        message: '',
+        nomKo: false,
+        prenomKo: false,
       }
     },
     methods: {
       traitement() {
-        console.log(this.nom, this.civilite);
+        //test si 2 caractères mini pour nom et prenom
+        if(this.nom.length >= 2) this.nomKo = false;
+        else this.nomKo = true;
+        if(this.prenom.length < 2 ) this.prenomKo = true;
+        else this.prenomKo = false;
+//        console.log(this.nom, this.civilite);
+        if(this.nomKo === false && this.prenomKo === false) this.message = 'Bonjour ' + this.prenom + ' ' + this.nom;
       }
     }
   }
 </script>
 
 <style scoped>
-
+.ko {
+  border-color: #FF5555;
+}
+span.ko {
+  background-color: #FF5555;
+  color: white;
+  padding: 2px;
+}
 </style>
