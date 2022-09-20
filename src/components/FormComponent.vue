697d282abb3f5fbf28f8f33db47e72893c4a7f9e
Bruno-TAVERNIER <bruno@tavernier.eu>
2022-09-20 14:58:51 +0200

    ajout watch

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="form">
     <form @submit.prevent="traitement"> 
-      <p>
+      <div>
         <label>Nom</label>
         <!-- v-model : liaison bi-directionnelle html<->JS 
              v-model.lazy : mise à jour sur le blur plutot que sur le change 
              v-model.trim : suppression des espaces avant et après le texte
              v-model.number : nombre uniquement
         -->
-        <input v-model.lazy="nom" placeholder="votre nom ici" :class='{ko: nomKo}' />
-        <span v-show="nomKo" class="ko">Votre nom doit comporter au moins 2 caractères...</span>
-      </p>
-      <p>
+        <input v-model="nom" placeholder="votre nom ici" :class='{ko: nomKo}' />
+        <p v-show="nomKo" class="ko">Votre nom doit comporter au moins 2 caractères...</p>
+      </div>
+      <div>
         <label>Prénom</label>
         <input v-model="prenom" placeholder="votre prénom ici" :class='{ko: prenomKo}' />
-        <span v-show="prenomKo" class="ko">Votre prénom doit comporter au moins 2 caractères...</span>
-      </p>
+        <p v-show="prenomKo" class="ko">Votre prénom doit comporter au moins 2 caractères...</p>
+      </div>
       <p>
         <label>Civilité</label>
         <select v-model="civilite">
@@ -23,17 +23,21 @@
           <option value="f">Madame</option>
           <option value="m">Monsieur</option>
         </select>
-        <span> {{ civilite }} </span>
+        <!-- appel de la fonction testCivilite directement dans le template -->
+        <span> {{ testCivilite }} </span>
       </p>
       <p>
         <button type="submit">Go !</button>
       </p>
       <p>{{ message }}</p>
     </form>
+    <!-- appel de la fonction maDate (mise en cache)-->
+    <p>{{ maDate }}</p>
   </div>
 </template>
 
 <script>
+
   export default {
     name: 'FormComponent',
     data() {
@@ -46,17 +50,48 @@
         prenomKo: false,
       }
     },
+    watch: {
+      //fonction pour surveiller la variable nom
+      //2 params: la nouvelle valeur et l'ancienne
+      nom(newVal, oldVal) {
+        console.log(oldVal, newVal);
+        if(newVal.length < 2 ) this.nomKo = true;
+        else this.nomKo = false;
+      },
+      prenom(nVal) {
+        if(nVal.length < 2 ) this.prenomKo = true;
+        else this.prenomKo = false;
+      }
+    },
+    /* fonctions avec mise en cache, sans params et obligatoirement valeur de retour */
+    computed: {
+      testCivilite() {
+        let retour;
+        if(this.civilite == 'f') retour = 'Madame';
+        else if(this.civilite == 'm') retour = 'Monsieur';
+        else retour = '';
+        return retour; 
+      },
+      maDate() {
+        return new Date().getFullYear();
+      }
+    },
     methods: {
       traitement() {
         //test si 2 caractères mini pour nom et prenom
-        if(this.nom.length >= 2) this.nomKo = false;
+       /* if(this.nom.length >= 2) this.nomKo = false;
         else this.nomKo = true;
         if(this.prenom.length < 2 ) this.prenomKo = true;
-        else this.prenomKo = false;
+        else this.prenomKo = false;*/
 //        console.log(this.nom, this.civilite);
-        if(this.nomKo === false && this.prenomKo === false) this.message = 'Bonjour ' + this.prenom + ' ' + this.nom;
+        if(this.nomKo === false && this.prenomKo === false) 
+          this.message = 'Bonjour ' 
+                          + this.prenom 
+                          + ' ' 
+                          + this.nom;
       }
-    }
+    },
+    
   }
 </script>
 
@@ -64,7 +99,7 @@
 .ko {
   border-color: #FF5555;
 }
-span.ko {
+p.ko {
   background-color: #FF5555;
   color: white;
   padding: 2px;
